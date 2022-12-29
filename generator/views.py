e8a7627b7f96edfe3bd4924c6da0b52abde829a3
galileomartinez98 <galioblock@gmail.com>
2022-12-28 20:56:47 -0600

    agrengando numeros mayuscula y especiales a home

--- generator/views.py
+++ generator/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.shortcuts import HttpResponse
-
+import random
 # Create your views here.
 
 def about(request):
@@ -10,4 +10,24 @@ def home(request):
     return render(request, 'generator/home.html')
 
 def password(request):
-    return render(request, 'generator/password.html')
+    character = list('abcdefghIjklmnopqrstuvwxyz')
+    generated_password = ''
+
+    length = int(request.GET.get('length'))
+
+    #MAYUS DFHUFEHW
+    if request.GET.get('uppercase'):
+        character.extend(list('ABCDEFGHJIKLMNIOPQRSTUVWXYZ'))
+
+    #SPECIAL $%(
+    if request.GET.get('special'):
+        character.extend(list('°!"#$%&/()=?¿}[{]+-*].,-_<>'))
+
+    #NUMBERS 164
+    if request.GET.get('numbers'):
+        character.extend(list('1234567890'))
+
+    for x in range(length):
+        generated_password += random.choice(character)
+
+    return render(request, 'generator/password.html', {'password': generated_password})
